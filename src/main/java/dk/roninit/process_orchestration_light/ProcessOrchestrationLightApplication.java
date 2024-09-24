ff4d8f5dc1b8d7f1eef6e85e874e8ea7144fbf59
Kasper Odgaard <kodga@nets.eu>
2024-09-24 09:47:11 +0200

    spring initializr

---
+++ src/main/java/dk/roninit/process_orchestration_light/ProcessOrchestrationLightApplication.java
@@ -0,0 +1,13 @@
+package dk.roninit.process_orchestration_light;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessOrchestrationLightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessOrchestrationLightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/roninit/process_orchestration_light/ProcessOrchestrationLightApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.roninit.process_orchestration_light;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessOrchestrationLightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
